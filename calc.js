a1c882ab0d6c955d43130aa3ade433c3865fc5e9
Vee0601 <gadisivhuhwavho@gmail.com>
2023-06-21 19:26:08 +0200

    first commit

---
+++ calc.js
@@ -0,0 +1,24 @@
+const theNumber = document.querySelector(".theNumber");
+const incrementBtn = document.querySelector(".increment");
+const decrementBtn = document.querySelector(".decrement");
+
+
+let myCounter = 4;
+theNumber.innerText = myCounter;
+
+incrementBtn.addEventListener('click', function(){
+// alert("+");
+myCounter++;
+theNumber.innerText = myCounter;
+   
+});
+
+decrementBtn.addEventListener('click', function(){
+//    alert("-");
+    if(myCounter > 0){
+
+        myCounter--;
+    theNumber.innerText = myCounter;
+    }
+    
+});
